75acb7e02b4e8b1d4b22cf34ad4c7068f057e569
hessamhashemi <hessam.h.ir@gmail.com>
2021-05-11 11:16:09 +0430

    change download status check and try for failed download

--- filedownload.go
+++ filedownload.go
@@ -3,111 +3,66 @@ package filedownload
 import (
 	"errors"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
 type urlInfo struct {
-	url string
-	fileLength int64
-	fileName string
-	etag string
+	url         string
+	fileLength  int64
+	fileName    string
+	etag        string
 	connSuccess bool
-	statusCode int
+	statusCode  int
 }
 
 type Chunk struct {
 	Begin int64
-	End int64
+	End   int64
 }
 
 type Downloader struct {
-	Timeout time.Duration
-	fileName string
-	fileSize int64
-	chunk	[]Chunk
-	chunkSize int64
+	Timeout             time.Duration
+	fileName            string
+	fileSize            int64
+	chunk               []Chunk
+	chunkSize           int64
 	numberOfConnections int
-	numberOfChunks int
-	uri	string
+	numberOfChunks      int
+	uri                 string
+	info                urlInfo
+}
+type status struct {
+	success bool
+	error   error
 }
 
 type funcFeedback struct {
-	id	int
-	chunk	Chunk
+	id     int
+	chunk  Chunk
 	cursor int64
+	stat   status
 }
 
-func NewDownloader() *Downloader {
-	return &Downloader{
-		Timeout: 15 * time.Second,
-	}
-}
+func Download(uri string, nChunk int) error {
+	var feedbacks []*funcFeedback
 
-func Download(uri string, nc int) error {
 	d := &Downloader{
 		uri:            uri,
 		Timeout:        15 * time.Second,
-		numberOfChunks: nc,
+		numberOfChunks: nChunk,
 	}
 	available := make(chan bool)
-	done := make(chan bool)
+	//done := make(chan bool)
 	feedBack := make(chan *funcFeedback)
-
-	downloadChunk := func(file *os.File, chunkIndex int) {
-		for {
-			<-available
-
-		client := http.Client{}
-
-		req, err := http.NewRequest("GET", d.uri, nil)
-		if err != nil {
-			return
-		}
-
-		req.Header.Add(
-			"Range",
-			fmt.Sprintf("bytes=%d-%d", d.chunk[chunkIndex].Begin, d.chunk[chunkIndex].End),
-		)
-
-		response, err := client.Do(req)
-		defer func() {
-			_ = response.Body.Close()
-		}()
-
-		buf := make([]byte, d.chunkSize+2)
-		cursor := d.chunk[chunkIndex].Begin
-
-		n, err := io.ReadFull(response.Body, buf)
-		if err == io.EOF {
-			log.Fatal(err)
-		}
-
-		_, errWr := file.WriteAt(buf[:n], cursor)
-		if errWr != nil {
-			log.Fatal(err)
-			break
-		}
-		feedBack <- &funcFeedback{
-			id:     chunkIndex,
-			chunk:  d.chunk[chunkIndex],
-			cursor: cursor,
-		}
-
-		done <- true
-		break
-	}
-}
 
 	info, err := gatherFileInfo(d)
 	if err != nil {
 		return err
 	}
-	d.fileName = info.fileName
-	d.fileSize = info.fileLength
+	//d.fileName = info.fileName
+	//d.fileSize = info.fileLength
 
 	_, errF := setupFile(d)
 	if errF != nil {
@@ -130,23 +85,30 @@ func Download(uri string, nc int) error {
 		return err
 	}
 
-
-	for i:=0 ; i < d.numberOfChunks; i++ {
-		go downloadChunk(file, i)
+	for i := 0; i < nChunk; i++ {
+		go downloadChunk(d, file, i, available, feedBack)
 		available <- true
 	}
 
-	count := nc
+	count := nChunk
 
-	r: for {
+r:
+	for {
 		select {
-			case <-done:
-				count--
-				if count == 0 {
-					break r
-				}
-			case f:= <- feedBack:
-				fmt.Printf("%+v\n", f)
+		case f := <-feedBack:
+			count--
+			if count == 0 {
+				break r
+			}
+			feedbacks = append(feedbacks, f)
+			fmt.Printf("%+v\n", f)
+		}
+	}
+
+	//check if chunk download fail run again
+	for _, feedback := range feedbacks {
+		if !feedback.stat.success {
+			go downloadChunk(d, file, feedback.id, available, feedBack)
 		}
 	}
 	return nil
