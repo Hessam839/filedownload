5135dc4575aa212b21cea7ea940105be1876e79f
Hessam Hashemi <hessam.h.ir@gmail.com>
2021-05-11 18:14:03 +0430

    add directory to file path

--- setupfile.go
+++ setupfile.go
@@ -1,9 +1,15 @@
 package filedownload
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
 
 func setupFile(d *Downloader) (os.FileInfo, error) {
-	file, err := os.Create(d.fileName)
+	file, err := os.Create(
+		filepath.Join(d.dir,
+			d.fileName))
+
 	if err != nil {
 		return nil, err
 	}
